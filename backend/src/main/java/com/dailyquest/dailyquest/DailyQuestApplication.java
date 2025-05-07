9dd20d23ede3ac66e21baa853a0727df662f2bcb
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-07 12:10:07 -0400

    backend initial commit

---
+++ backend/src/main/java/com/dailyquest/dailyquest/DailyQuestApplication.java
@@ -0,0 +1,13 @@
+package com.dailyquest.dailyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyQuestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dailyquest/dailyquest/DailyQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyquest.dailyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
