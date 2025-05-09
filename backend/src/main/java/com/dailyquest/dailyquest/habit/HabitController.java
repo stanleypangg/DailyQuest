1e3aa0ff3fdee95cdd942ddac1116cbaafcd33b2
Andrew Ho <andrewwhho@gmail.com>
2025-05-09 14:06:11 -0400

    api mapping for habits

--- backend/src/main/java/com/dailyquest/dailyquest/habit/HabitController.java
+++ backend/src/main/java/com/dailyquest/dailyquest/habit/HabitController.java
@@ -1,7 +1,6 @@
 package com.dailyquest.dailyquest.habit;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- backend/src/main/java/com/dailyquest/dailyquest/habit/HabitModel.java
+++ backend/src/main/java/com/dailyquest/dailyquest/habit/HabitModel.java
@@ -4,7 +4,6 @@ import com.dailyquest.dailyquest.userprofile.UserProfileModel;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 public class HabitModel {
