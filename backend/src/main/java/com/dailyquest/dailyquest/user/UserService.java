4968d144f8448ffb33dc3b527cfc55025d25361b
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-09 13:59:10 -0400

    Create SecurityConfig for password encoder

---
+++ backend/src/main/java/com/dailyquest/dailyquest/user/UserService.java
@@ -0,0 +1,18 @@
+package com.dailyquest.dailyquest.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
