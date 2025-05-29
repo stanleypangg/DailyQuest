8241bc68dd490393367a0d43dc13266ada85b79b
Andrew Ho <andrewwhho@gmail.com>
2025-05-28 21:22:45 -0400

    CONTACT ROUTING

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router'
 import About from './components/about'
 import Home from './components/home'
+import Contact from './components/contact'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home /> } />
       <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact /> } />
     </Routes>
   )
 }
---
+++ frontend/src/components/contact/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      contact page
+    </div>
+  )
+}
+
+export default index
