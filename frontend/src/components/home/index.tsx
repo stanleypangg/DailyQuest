fa422165fa3247141d6f52a94eb40aa1959d7a3a
Andrew Ho <andrewwhho@gmail.com>
2025-06-04 12:13:25 -0400

    added nav bar

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>
-      home
+    <div className='header'>
+      <div className='container'>
+        <nav>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/about'>About</a></li>
+            <li><a href='/contact'>Contact</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
