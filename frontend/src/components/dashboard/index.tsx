a69e1c7298a64e57f10fa178b118adc98d0491d3
Andrew Ho <andrewwhho@gmail.com>
2025-06-29 19:43:50 -0400

    created css grid for dashboard

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -7,15 +7,17 @@ const index = () => {
         <div id='grid-container' className='grid grid-cols-[260px_1fr_1fr_1fr] grid-rows-[0.2fr_3fr] h-screen'>
 
             {/* Header */}
-            <header id='header' className='h-[70px] bg-blue-400'>
+            <header id='header' className='h-[70px] bg-gray-400 flex items-center justify-between p-[0_30px_0_30px] shadow-[0_6px_7px_-3px_rgba(0,0,0,0.35)]'>
+                <div> 
+                </div>
             </header>
 
             {/* Sidebar */}
-            <aside id='sidebar' className='h-max bg-gray-500'>
+            <aside id='sidebar' className='h-full bg-gray-300 overflow-y-auto transition-all duration-[0.5s]'>
             </aside>
 
             {/* Main */}
-            <main id='main-container' className='bg-orange-200'>
+            <main id='main-container' className='bg-gray-200 overflow-y-auto p-[20px_20px] '>
             </main>
 
         </div>
